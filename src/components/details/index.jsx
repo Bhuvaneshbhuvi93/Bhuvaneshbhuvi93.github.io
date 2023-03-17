443b8da1b2a5a5f84b702bc218f347e9d9d1c0d5
Bhuvaneshwar <118096816+Bhuvaneshbhuvi93@users.noreply.github.com>
2023-03-17 19:07:01 +0530

    Update index.jsx

--- src/components/details/index.jsx
+++ src/components/details/index.jsx
@@ -22,7 +22,6 @@ import {
 } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { skeleton } from '../../helpers/utils';
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
