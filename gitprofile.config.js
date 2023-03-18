f801168d0b2c0c699daf288b576869d0da7b252c
Bhuvaneshwar <118096816+Bhuvaneshbhuvi93@users.noreply.github.com>
2023-03-18 13:55:27 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -17,7 +17,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1V9gB6eVUUGl9MAmZ3dOkBrI1EZtu9ZD4/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
