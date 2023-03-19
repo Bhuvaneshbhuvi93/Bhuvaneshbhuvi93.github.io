2e6360477e26c7e5d1d704cb23827fce70411ba9
Bhuvaneshwar <118096816+Bhuvaneshbhuvi93@users.noreply.github.com>
2023-03-19 14:17:16 +0530

    Update index.jsx

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -135,8 +135,7 @@ const Project = ({ repo, loading, github, googleAnalytics }) => {
                       skeleton({ width: 'w-40', height: 'h-8' })
                     ) : (
                       <span className="text-base-content opacity-70">
-//                         GitHub Projects
-                        GitHub Repository
+                        Project Repository GitHub
                       </span>
                     )}
                   </h5>
