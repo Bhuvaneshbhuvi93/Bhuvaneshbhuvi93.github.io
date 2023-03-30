9b39e88088e53f65e5a7e339cb1f138b2f3743f4
Bhuvaneshwar <118096816+Bhuvaneshbhuvi93@users.noreply.github.com>
2023-03-30 06:42:06 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -87,29 +87,24 @@ const config = {
       title: 'PhonePe Pulse Data Visualization',
       description:
         'This project included interative visualization of PhonePe pulse data in the streamlit GUI. Technologies used are Github Cloning, Python, Pandas, MySQL, mysql-connector-python, Streamlit, and Plotly.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
     },
     {
       title: 'Twitter Scrapping',
       description:
         "This project allows you to collect tweets for a specific keyword or hashtag, within a given date range and a limit of tweets to be scraped. The project is built using Python and the popular libraries 'streamlit', 'pandas', and 'pymongo'.",
-      imageUrl:
-      link: 
+
     },
     {
       title: 'Calorie Counting Using LInear Regression',
       description:
         "For this project, I have worked on developing a machine learning model that predicts the number of calories burned by a person based on their steps taken, active minutes, and distance travelled. The model has been trained on real-life fitness data, giving us impressive results in terms of accuracy. This project has been a great learning experience.",
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+
     },
     {
       title: 'Extracting Business Card Data with OCR',
       description:
         "This project allows you to collect informaion from any image or document uploded into the tool the tool was build using the technologies OCR,streamlit GUI, SQL,Data Extraction",
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+
     },
     
   ],
