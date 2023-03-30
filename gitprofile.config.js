8ac5d5636cf86a39814c1157f7e54cfb6184c2dd
Bhuvaneshwar <118096816+Bhuvaneshbhuvi93@users.noreply.github.com>
2023-03-30 06:40:16 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -94,8 +94,8 @@ const config = {
       title: 'Twitter Scrapping',
       description:
         "This project allows you to collect tweets for a specific keyword or hashtag, within a given date range and a limit of tweets to be scraped. The project is built using Python and the popular libraries 'streamlit', 'pandas', and 'pymongo'.",
-      imageUrl:,
-      link:,
+      imageUrl:
+      link: 
     },
     {
       title: 'Calorie Counting Using LInear Regression',
